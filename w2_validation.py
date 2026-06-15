dec5b475e752a492047f2ea20cf4f09d19da1b2c
hasan <alansary227@gmail.com>
2026-06-15 11:33:57 +0100

    add sliced W2 validation script

---
+++ w2_validation.py
@@ -0,0 +1,35 @@
+from samplers import rejection_sampling
+from utils import compute_w2, compute_sliced_w2
+from models import classifier, StyleGAN2Wrapper
+
+import pickle
+import torch
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f'Using device {device}')
+
+smile_clf = classifier()
+smile_clf.load_state_dict(torch.load('./clf_checkpoints/smile_clf.pth', map_location=device, weights_only=False))
+smile_clf.eval()
+
+with open('stylegan2_checkpoints/celebahq-res256-mirror-paper256-kimg100000-ada-target0.5.pkl', 'rb') as f:
+    G = pickle.load(f)['G_ema'].to(device)
+    
+stylegan = StyleGAN2Wrapper(G).to(device)
+stylegan.eval()
+
+# get rejection sampling samples
+rs_1 = rejection_sampling(stylegan, smile_clf, 1000, device)
+rs_2 = rejection_sampling(stylegan, smile_clf, 1000, device)
+
+#compute w2 (exact)
+rs_w2 = compute_w2(rs_1, rs_2)
+
+w2_slices = {50: None, 100: None, 150: None, 200: None, 250: None, 300: None, 350: None, 400: None, 450: None, 500: None}
+
+for slice in w2_slices:
+    w2_slices[slice] = compute_sliced_w2(rs_1, rs_2, slice)
+
+print(f"The exact W2: {rs_w2}")
+
+for k, v in w2_slices.items():
+    print(f"# slices: {k} ==> sliced_w2: {v}")
