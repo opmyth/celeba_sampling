ab7e6c2df173f470321c7afd9cb81931a28a756f
hasan <alansary227@gmail.com>
2026-06-18 18:04:38 +0100

    revert to landonia11 A6000; saxa H200 has fabric manager issue

--- submit_bench.sh
+++ submit_bench.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=mala_bench
 #SBATCH -p Teaching
 #SBATCH --account=general-teaching
-#SBATCH --gres=gpu:h200:1
-#SBATCH --nodelist=saxa
+#SBATCH --gres=gpu:1
+#SBATCH --exclude=saxa,opencast,damnii[07-12],landonia01,landonia02,landonia03,landonia05,landonia08,landonia23,landonia25
 #SBATCH --time=01:30:00
 #SBATCH --output=logs/mala_bench-%j.out
 #SBATCH --error=logs/mala_bench-%j.err
@@ -14,8 +14,10 @@ echo "Started: $(date -u)"
 
 module load cuda/12.8.0
 export CUDA_HOME=/opt/cuda-12.8.0
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}"
+. /home/htang2/toolchain-20251006/toolchain.rc
 . /home/s2800722/venv/bin/activate
-export LD_LIBRARY_PATH=/home/s2800722/venv/lib/python3.12/site-packages/torch/lib:/home/s2800722/venv/lib/python3.12/site-packages/nvidia/cuda_runtime/lib:/opt/cuda-12.8.0/lib64:${LD_LIBRARY_PATH:-}
+export LD_LIBRARY_PATH=/home/s2800722/venv/lib/python3.12/site-packages/torch/lib:/home/s2800722/venv/lib/python3.12/site-packages/nvidia/cuda_runtime/lib:$LD_LIBRARY_PATH
 
 export TORCH_EXTENSIONS_DIR=/tmp/torch_ext_${SLURM_JOB_ID}
 
