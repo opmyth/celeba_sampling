a560b7fd47f085a8f0858d5523c2fd1360c6cc88
hasan <alansary227@gmail.com>
2026-07-03 18:45:13 +0100

    fix male_eye: use A6000, allow landonia11

--- scripts/submit_male_eye_pipeline.sh
+++ scripts/submit_male_eye_pipeline.sh
@@ -3,7 +3,7 @@
 #SBATCH -p Teaching
 #SBATCH --account=general-teaching
 #SBATCH --gres=gpu:nvidia_rtx_a6000:1
-#SBATCH --exclude=opencast,landonia11,damnii07
+#SBATCH --exclude=opencast,damnii07
 #SBATCH --time=08:00:00
 #SBATCH --output=logs/male_eye_pipeline-%j.out
 #SBATCH --error=logs/male_eye_pipeline-%j.err
